1e7592673db3c263c894e4a201138b6a65199511
neooffline <admin@neooffline.ru>
2019-03-04 20:06:45 +0300

    Auto format code

--- src/ru/neooffline/j2l2/MyArraySum.java
+++ src/ru/neooffline/j2l2/MyArraySum.java
@@ -1,28 +1,31 @@
 package ru.neooffline.j2l2;
 
-public class MyArraySum extends Exception{
+public class MyArraySum extends Exception {
     private final int ARRAY_LENGTH = 4;
     private int sum;
-    public MyArraySum(){
+
+    public MyArraySum() {
         this.sum = 0;
     }
-    public int getSum(String[][] strArray){
+
+    public int getSum(String[][] strArray) {
         checkArray(strArray);
-            for (int i = 0; i < strArray.length; i++) {
-                for (int j = 0; j < strArray[i].length; j++) {
-                    try {
-                        sum += Integer.parseInt(strArray[i][j]);
-                    } catch (NumberFormatException ex){
-                        throw new MyArrayDataException("в €чейке: " + (i+1) +" - " + (j+1) + " несоответсвующий тип данных");
-                    }
+        for (int i = 0; i < strArray.length; i++) {
+            for (int j = 0; j < strArray[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(strArray[i][j]);
+                } catch (NumberFormatException ex) {
+                    throw new MyArrayDataException("в €чейке: " + (i + 1) + " - " + (j + 1)
+                            + " несоответсвующий тип данных");
                 }
             }
-            return sum;
+        }
+        return sum;
     }
 
-    private void checkArray(String[][] strArray){
+    private void checkArray(String[][] strArray) {
         for (int i = 0; i < strArray.length; i++) {
-            if (strArray[i].length != this.ARRAY_LENGTH){
+            if (strArray[i].length != this.ARRAY_LENGTH) {
                 throw new MyArraySizeException("ошибка длины массива");
             }
         }
